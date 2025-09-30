4aa51c99bd4af04d3b75fddea37419879a5f4e65
jeffye <3420661@qq.com>
2025-09-30 18:08:48 +0800

    update build method

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='yelib',
-    packages=find_packages(),
-    version='0.1.1',
-    install_requires=[  # 添加了依赖的 package
-        'numpy'
-    ],
-    entry_points={
-        "console_scripts": [
-            "yelib = yelib.__main__:entry_point",
-            "gelib = yelib.__main__:main",
-        ]
-    },
-)
+setup()  # 所有配置都在 pyproject.toml 中定义
